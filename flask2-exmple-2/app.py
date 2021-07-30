dfcc1c0bc629f824fe0fd23593312c0667b97626
markomitrovic335 <markomitrovic335@gmail.com>
2021-07-30 14:33:44 +0200

    ad

--- flask2-exmple-2/app.py
+++ flask2-exmple-2/app.py
@@ -2,15 +2,31 @@ import toml
 
 from flask import Flask
 
+
 app = Flask('__name__')
 
-@app.route('/', methods=['GET'])
-def server_running():
-    return 'Server running. . .'
 
-@app.route('/home', methods=['GET'])
-def home_page():
-    return 'This is home page. . .'
+# @app.route('/', methods=['GET'])
+# def server_running():
+#     return 'Server running. . .'
 
 
-app.config.from_file('config.toml', toml.load)
+# @app.route('/home', methods=['GET'])
+# def home_page():
+#     return 'This is home page. . .'
+
+
+@app.get('/first')
+def my_get_endpoint():
+    return 'This was a GET request.'
+
+
+@app.post('/second')
+def my_post_endpoint():
+    return 'This was a POST request.'
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=1989, debug=True)
+
+# app.config.from_file('config.toml', toml.load)
