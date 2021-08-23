e89b4b33908a30398415bb5285e233495222a25d
markomitrovic335 <markomitrovic335@gmail.com>
2021-08-23 09:45:52 +0200

    #flask - created few more routes

--- flask2-example4/routes/user_routes.py
+++ flask2-example4/routes/user_routes.py
@@ -38,6 +38,42 @@ async def create():
     return jsonify(response_dict)
 
 
+# login
+@user_bp.post('/login')
+async def login():
+    
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    
+    async with Session() as session:
+        try: 
+            # Get user by id
+            user = await session.execute(select(User).where(User.email == email))
+            user = user.scalar()
+            
+            if not user.password == password:
+                return jsonify({'error': 'Wrong credentials!'}), 400
+            
+            response_dict = {
+                'user':{
+                    'id': user._id,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                    'password': user.password
+                }
+            } 
+
+            await session.flush()
+            await session.commit()
+            await session.close()
+        except:
+            return jsonify({'error': 'User not found! - Wrong email address'}), 404
+    
+    return jsonify(response_dict)
+
+
 # get all
 @user_bp.post('/all')
 async def get_all():
@@ -126,4 +162,57 @@ async def remove_user():
         'data': 'User deleted!'
     })
     
+
+# update
+@user_bp.post('/update')
+async def update():
     
+    args = request.args
+    id = args.get('id')
+    
+    if not id:
+        return jsonify({'error': 'User id is required'}), 400 
+    
+    async with Session() as session:
+        
+        # Get user by id
+        user = await session.execute(select(User).where(User._id == id))
+        user = user.scalar() 
+
+        if not user:
+            return jsonify({
+                'error': f'User with id: {id} not found'
+            }), 404
+            
+        data = request.get_json()    
+            
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        new_email = data.get('email')
+        
+        if new_email:
+            check_user = await session.execute(select(User).where(User.email == new_email))
+            check_user = check_user.scalar()
+
+            if check_user:
+                return jsonify({
+                    'error': 'Email already exists, change email and try again'
+                }), 400
+            else:
+                user.email = new_email
+    
+        session.add(user)
+        await session.commit()
+        await session.close()
+        
+        response_dict = {
+                'user':{
+                    'id': user._id,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                    'password': user.password
+                }
+            } 
+
+    return jsonify({'data': response_dict}), 200
